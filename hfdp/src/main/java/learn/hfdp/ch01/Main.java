aa680bd7fe2370eb0c51eb8bc40609139907b5a9
Denis Popkov <pkvdenis@gmail.com>
2018-09-10 16:32:34 +0300

    hfdp: ch01: add method fly() to abstract class Duck.

--- hfdp/src/main/java/learn/hfdp/ch01/Main.java
+++ hfdp/src/main/java/learn/hfdp/ch01/Main.java
@@ -2,13 +2,15 @@ package learn.hfdp.ch01;
 
 public class Main {
     public static void main(String[] args) {
-        Duck[] ducks = new Duck[2];
+        Duck[] ducks = new Duck[3];
         ducks[0] = new MallardDuck();
         ducks[1] = new RedheadDuck();
+        ducks[2] = new RubberDuck();
         for (Duck duck : ducks) {
             duck.display();
             duck.quack();
             duck.swim();
+            duck.fly();
         }
     }
 }
