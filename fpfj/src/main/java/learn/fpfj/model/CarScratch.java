f4bc75713b4c602a2965ef9c260e1526bffb22cc
Denis Popkov <pkvdenis@gmail.com>
2018-11-08 17:45:47 +0300

    fpfj: 3.6 Cleaning up the design (turn static functions into instance).

--- fpfj/src/main/java/learn/fpfj/model/CarScratch.java
+++ fpfj/src/main/java/learn/fpfj/model/CarScratch.java
@@ -1,37 +1,8 @@
 package learn.fpfj.model;
 
-import java.time.LocalDate;
 import java.util.*;
 
-class PassengerCountOrder implements Comparator<Car> {
-    @Override
-    public int compare(Car o1, Car o2) {
-        return o1.getPassengers().size() - o2.getPassengers().size();
-    }
-}
-
-@FunctionalInterface
-interface Criterion<E> {
-    boolean test(E e);
-}
-
-interface Strange {
-    boolean stuff(Car c);
-}
-
 public class CarScratch {
-    public static <T> Criterion<T> negate(Criterion<T> crit) {
-        return t -> !crit.test(t);
-    }
-
-    public static <T> Criterion<T> and(Criterion<T> first, Criterion<T> second) {
-        return t -> first.test(t) && second.test(t);
-    }
-
-    public static <T> Criterion<T> or(Criterion<T> first, Criterion<T> second) {
-        return t -> first.test(t) || second.test(t);
-    }
-
     public static <T> void showAll(List<T> lc) {
         for (T c : lc) {
             System.out.println(c);
@@ -62,22 +33,18 @@ public class CarScratch {
         showAll(getByCriterion(cars, Car.getRedCarCriterion()));
         showAll(getByCriterion(cars, Car.getGasLevelCarCriterion(6)));
 
-        /*showAll(getByCriterion(cars, Car.getGasLevelCarCriterion(7)));
-        showAll(getByCriterion(cars, Car.getGasLevelCarCriterion(4)));
-        showAll(getByCriterion(cars, Car.getColorCriterion("Green", "Red")));*/
-
         Criterion<Car> level7 = Car.getGasLevelCarCriterion(7);
         showAll(getByCriterion(cars, level7));
-        Criterion<Car> notLevel7 = negate(level7);
+        Criterion<Car> notLevel7 = level7.negate();
         showAll(getByCriterion(cars, notLevel7));
 
         Criterion<Car> isRed = Car.getColorCriterion("Red");
         Criterion<Car> fourPassengers = Car.getFourPassengerCriterion();
-        Criterion<Car> redFourPassengers = and(isRed, fourPassengers);
+        Criterion<Car> redFourPassengers = isRed.and(fourPassengers);
         showAll(getByCriterion(cars, redFourPassengers));
 
         Criterion<Car> isBlack = Car.getColorCriterion("Black");
-        Criterion<Car> blackOrFourPassengers = or(isBlack, fourPassengers);
+        Criterion<Car> blackOrFourPassengers = isBlack.or(fourPassengers);
         showAll(getByCriterion(cars, blackOrFourPassengers));
     }
 }
