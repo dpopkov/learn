e66ee2d6563e407d13d9229ba0ca487e6372e3ce
Denis Popkov <pkvdenis@gmail.com>
2018-12-19 15:21:57 +0300

    dsai: ch08: Binary Trees: Binary search tree, add pseudo graphic.

--- dsai/src/main/java/learn/dsai/ch08trees/TreeLong.java
+++ dsai/src/main/java/learn/dsai/ch08trees/TreeLong.java
@@ -1,5 +1,7 @@
 package learn.dsai.ch08trees;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.StringJoiner;
 import java.util.function.LongConsumer;
 
@@ -8,6 +10,8 @@ import java.util.function.LongConsumer;
  * This tree allows duplicate values.
  */
 public class TreeLong {
+    private static final String DOTS = "................................................................";
+
     private NodeLong root;
 
     public NodeLong find(long key) {
@@ -153,5 +157,59 @@ public class TreeLong {
             }
         }
         return current;
+    }
+
+    /**
+     * Builds string representation of the tree as pseudo graphic.
+     * Allows up to 5 levels of tree. If there are more levels, then
+     * the values at level greater than 5 will not be separated.
+     * @return pseudo graphic representation of the tree
+     */
+    public String buildForDisplay() {
+        StringBuilder builder = new StringBuilder();
+        Deque<NodeLong> globalStack = new LinkedList<>();
+        globalStack.push(root);
+        int nBlanks = 32;
+        boolean isRowEmpty = false;
+        appendLine(builder);
+        while (!isRowEmpty) {
+            Deque<NodeLong> localStack = new LinkedList<>();
+            isRowEmpty = true;
+            appendIndentation(builder, nBlanks);
+            while (!globalStack.isEmpty()) {
+                NodeLong temp = globalStack.pop();
+                if (temp != null) {
+                    builder.append(temp.data);
+                    localStack.push(temp.left);
+                    localStack.push(temp.right);
+                    if (temp.left != null || temp.right != null) {
+                        isRowEmpty = false;
+                    }
+                } else {
+                    builder.append("--");
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+                appendIndentation(builder, nBlanks * 2 - 2);
+            }
+            builder.append(System.lineSeparator());
+            nBlanks /= 2;
+            while (!localStack.isEmpty()) {
+                globalStack.push(localStack.pop());
+            }
+        }
+        appendLine(builder);
+        return builder.toString();
+    }
+
+    private void appendLine(StringBuilder builder) {
+        builder.append(DOTS);
+        builder.append(System.lineSeparator());
+    }
+
+    private void appendIndentation(StringBuilder builder, int nBlanks) {
+        for (int j = 0; j < nBlanks; j++) {
+            builder.append(' ');
+        }
     }
 }
--- dsai/src/main/java/learn/dsai/ch08trees/TreeLongApp.java
+++ dsai/src/main/java/learn/dsai/ch08trees/TreeLongApp.java
@@ -1,16 +1,64 @@
 package learn.dsai.ch08trees;
 
+import java.util.Scanner;
+
 public class TreeLongApp {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         TreeLong tree = new TreeLong();
-        tree.insert(50);
-        tree.insert(25);
-        tree.insert(75);
-        NodeLong found = tree.find(25);
-        if (found != null) {
-            System.out.println("Found the node with key 25");
-        } else {
-            System.out.println("could not find node with key 25");
+        long[] values = {50, 25, 75, 12, 37, 43, 30, 33, 87, 93, 97};
+        tree.insert(values);
+        long value;
+        NodeLong node;
+        outerLoop: while (true) {
+            System.out.print("Enter first letter of show, ");
+            System.out.print("insert, find, delete, or traverse: ");
+            String choice = in.nextLine();
+            switch (choice) {
+                case "s":
+                    System.out.println(tree.buildForDisplay());
+                    break;
+                case "i":
+                    System.out.print("Enter value to insert: ");
+                    value = getLong(in);
+                    tree.insert(value);
+                    break;
+                case "f":
+                    System.out.print("Enter value to find: ");
+                    value = getLong(in);
+                    node = tree.find(value);
+                    if (node != null) {
+                        System.out.println("Found: " + node);
+                    } else {
+                        System.out.println("Could not find " + value);
+                    }
+                    break;
+                case "d":
+                    System.out.print("Enter value to delete: ");
+                    value = getLong(in);
+                    node = tree.find(value);
+                    if (node != null) {
+                        System.out.println("Deleted: " + node);
+                    } else {
+                        System.out.println("Could not find " + value);
+                    }
+                    break;
+                case "t":
+                    System.out.println("Tree: " + tree.toString());
+                    break;
+                case "x":
+                    System.out.println("Exiting.");
+                    break outerLoop;
+                default:
+                    System.out.println("Invalid entry");
+                    break;
+            }
         }
+    }
+
+    private static long getLong(Scanner in) {
+        long value = in.nextLong();
+        in.skip("\\s");
+        return value;
     }
 }
