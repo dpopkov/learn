f83a4ee8a4c04055f2495fd032516e523babff8e
Denis Popkov <pkvdenis@gmail.com>
2018-12-23 16:43:22 +0300

    dsai: ch08: Binary Trees: Projects: p.8.5 - finished Huffman code.

--- dsai/src/main/java/learn/dsai/ch08trees/projects/HuffmanTree.java
+++ dsai/src/main/java/learn/dsai/ch08trees/projects/HuffmanTree.java
@@ -6,21 +6,95 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 public class HuffmanTree {
-    public static void main(String[] args) {
-        /* Making Huffman tree. */
-        String s = "SUSIE SAYS IT IS EASY";
+    private static final int NUM_CHARACTERS = 28;
+
+    /**
+     * Table for characters A..Z, space, linefeed, where A is 0, B is 1,
+     * space is 26, and linefeed is 27.
+     */
+    private final String[] codes = new String[NUM_CHARACTERS];
+    private final Map<String, Character> codesToChars = new HashMap<>();
+
+    public HuffmanTree(String s) {
+        if (s == null || s.isEmpty()) {
+            throw new IllegalArgumentException("Input string is null or empty");
+        }
+        NodeFreqChar root = buildTree(s);
+        fillCodes(root, "");
+    }
+
+    public String encode(String s) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            builder.append(codes[getCodeIndex(ch)]);
+        }
+        return builder.toString();
+    }
+
+    public String decode(String s) {
+        StringBuilder builder = new StringBuilder();
+        int from = 0;
+        int to = 2;
+        while (to <= s.length()) {
+            String code = s.substring(from, to);
+            Character ch = codesToChars.get(code);
+            if (ch != null) {
+                builder.append(ch);
+                from = to;
+                to = from + 2;
+            } else {
+                to++;
+            }
+        }
+        return builder.toString();
+    }
+
+    private void fillCodes(NodeChar node, String code) {
+        if (node.left != null && node.right != null) {
+            fillCodes(node.left, code + "0");
+            fillCodes(node.right, code + "1");
+        } else {
+            codes[getCodeIndex(node.character)] = code;
+            codesToChars.put(code, node.character);
+        }
+    }
+
+    private int getCodeIndex(char ch) {
+        int codeIdx;
+        if ('A' <= ch && ch <= 'Z') {
+            codeIdx = ch - 'A';
+        } else if (ch == ' ') {
+            codeIdx = 26;
+        } else if (ch == '\n') {
+            codeIdx = 27;
+        } else {
+            throw new IllegalArgumentException("Character is not valid: " + ch);
+        }
+        return codeIdx;
+    }
+
+    private NodeFreqChar buildTree(String s) {
         Map<Character, Integer> frequencies = new HashMap<>();
         s.chars().forEach(ch -> frequencies.merge((char)ch, 1, (i, i2) -> i + 1));
         PriorityQueue<NodeFreqChar> queue = new PriorityQueue<>(Comparator.comparingInt(NodeFreqChar::getFrequency));
         frequencies.forEach((ch, freq) -> queue.add(new NodeFreqChar(ch, freq)));
-        NodeFreqChar n1 = queue.remove();
-        NodeFreqChar n2 = queue.remove();
-        NodeFreqChar n = new NodeFreqChar((char) 0, n1.getFrequency() + n2.getFrequency());
-        n.left = n1;
-        n.right = n2;
-        queue.add(n);
-        /*while (queue.size() > 1) {
-            // TODO: implement the rest
-        }*/
+        while (queue.size() > 1) {
+            NodeFreqChar leftNode = queue.remove();
+            NodeFreqChar rightNode = queue.remove();
+            int sumFrequency = leftNode.getFrequency() + rightNode.getFrequency();
+            NodeFreqChar node = new NodeFreqChar((char) 0, sumFrequency);
+            node.left = leftNode;
+            node.right = rightNode;
+            queue.add(node);
+        }
+        return queue.remove();
+    }
+
+    public static void main(String[] args) {
+        String s = "SUSIE SAYS IT IS EASY\n";
+        HuffmanTree tree = new HuffmanTree(s);
+        String encoded = tree.encode(s);
+        System.out.println(encoded);
     }
 }
--- dsai/src/main/java/learn/dsai/ch08trees/projects/P0805HuffmanCoding.java
+++ dsai/src/main/java/learn/dsai/ch08trees/projects/P0805HuffmanCoding.java
@@ -1,12 +1,37 @@
 package learn.dsai.ch08trees.projects;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class P0805HuffmanCoding {
-    public static void main(String[] args) {
-        // TODO: implement
-        // Accept a text message, possibly of more than one line.
-        // Create a Huffman tree for this message
-        // Create a code table
-        // Encode the message into binary
-        // Decode the message from binary back to text
+
+    public static void main(String[] args) throws IOException {
+        String message = acceptMessage(args);
+        HuffmanTree tree = new HuffmanTree(message);
+        String encoded = tree.encode(message);
+        System.out.println("encoded = " + encoded);
+        System.out.println("decoded = " + tree.decode(encoded));
+    }
+
+    private static String acceptMessage(String[] args) throws IOException {
+        String message = null;
+        if (args.length == 0) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter message: ");
+            message = in.nextLine();
+            in.close();
+        } else if (args.length == 1) {
+            message = args[0];
+        } else if ("--file".equals(args[0])) {
+            Path p = Paths.get(args[1]);
+            message = new String(Files.readAllBytes(p));
+        } else {
+            System.out.println("Invalid arguments");
+            System.exit(1);
+        }
+        return message;
     }
 }
