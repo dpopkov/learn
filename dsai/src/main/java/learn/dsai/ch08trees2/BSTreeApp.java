dc53037586e9e79997fc9a6b1bab89648f7e25f3
Denis Popkov <pkvdenis@gmail.com>
2019-01-09 20:47:03 +0300

    dsai: ch08_2nd: finish BSTreeApp.

--- dsai/src/main/java/learn/dsai/ch08trees2/BSTreeApp.java
+++ dsai/src/main/java/learn/dsai/ch08trees2/BSTreeApp.java
@@ -1,14 +1,60 @@
 package learn.dsai.ch08trees2;
 
+import java.util.Scanner;
+
 public class BSTreeApp {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         BSTree<Integer> tree = new BSTree<>();
-        tree.insert(50, 25, 75);
-        Integer n = tree.find(25);
-        if (n != null) {
-            System.out.println("Found the node with key 25");
-        } else {
-            System.out.println("Could not find node with key 25");
+        Integer[] values = {50, 25, 75, 12, 37, 43, 30, 33, 87, 93, 97};
+        final int cellWidth = 2;
+        tree.insert(values);
+        int value;
+        outerLoop: while (true) {
+            System.out.print("Enter first letter of show, ");
+            System.out.print("insert, find, delete, or traverse: ");
+            String choice = in.nextLine();
+            switch (choice) {
+                case "s":
+                    System.out.println(new TreeStringBuilder<>(tree.getRoot(), cellWidth).toString());
+                    break;
+                case "i":
+                    value = getInt("Enter value to insert: ", in);
+                    tree.insert(value);
+                    break;
+                case "f":
+                    value = getInt("Enter value to find: ", in);
+                    if (tree.find(value) != null) {
+                        System.out.println("Found: " + value);
+                    } else {
+                        System.out.println("Could not find " + value);
+                    }
+                    break;
+                case "d":
+                    value = getInt("Enter value to delete: ", in);
+                    if (tree.delete(value)) {
+                        System.out.println("Deleted: " + value);
+                    } else {
+                        System.out.println("Could not find " + value);
+                    }
+                    break;
+                case "t":
+                    System.out.println("Tree: " + tree.toString());
+                    break;
+                case "x":
+                    System.out.println("Exiting.");
+                    break outerLoop;
+                default:
+                    System.out.println("Invalid entry");
+                    break;
+            }
         }
+    }
+
+    private static int getInt(String prompt, Scanner in) {
+        System.out.print(prompt);
+        int value = in.nextInt();
+        in.nextLine();
+        return value;
     }
 }
