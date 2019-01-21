71a34d3efdb32f09872dba4c6802bc09ab06f9df
Denis Popkov <pkvdenis@gmail.com>
2019-01-21 22:39:52 +0300

    dsai: ch10: start projects.

---
+++ dsai/src/main/java/learn/dsai/ch10tree234/projects/P1001MinValue.java
@@ -0,0 +1,7 @@
+package learn.dsai.ch10tree234.projects;
+
+public class P1001MinValue {
+    public static void main(String[] args) {
+        System.out.println("Write a method that returns the minimum value in a 2-3-4 tree.");
+    }
+}
