8d72f7614963562e5bee46585224b3c86de9433d
Denis Popkov <pkvdenis@gmail.com>
2019-06-01 16:17:08 +0300

    dsajg6e: ch2 Object-Oriented Design: PredatoryCreditCard

---
+++ dsajg6e/src/main/java/learn/dsajg6e/ch02oop/PredatoryCreditCard.java
@@ -0,0 +1,37 @@
+package learn.dsajg6e.ch02oop;
+
+import learn.dsajg6e.ch01primer.CreditCard;
+
+/**
+ * CF 2.1
+ */
+@SuppressWarnings("unused")
+public class PredatoryCreditCard extends CreditCard {
+    private static final double ATTEMPTED_OVERCHARGE_PENALTY = 5.0;
+
+    /** Annual percentage rate. */
+    private final double apr;
+
+    public PredatoryCreditCard(String customer, String bank, String account, int limit,
+                               double balance, double rate) {
+        super(customer, bank, account, limit, balance);
+        apr = rate;
+    }
+
+    /** Access monthly interest charges. */
+    public void processMonth() {
+        if (balance > 0) {
+            double monthlyFactor = Math.pow(1 + apr, 1.0 / 12);
+            balance *= monthlyFactor;
+        }
+    }
+
+    @Override
+    public boolean charge(double price) {
+        boolean success = super.charge(price);
+        if (!success) {
+            balance += ATTEMPTED_OVERCHARGE_PENALTY;
+        }
+        return success;
+    }
+}
---
+++ dsajg6e/src/main/java/learn/dsajg6e/ch02oop/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <h3>2. Object-Oriented Design</h3>
+ *
+ * 2.1 Goals, Principles, and Patterns<br>
+ * 2.2 Inheritance<br>
+ * 2.3 Interfaces and Abstract Classes<br>
+ * 2.4 Exceptions<br>
+ * 2.5 Casting and Generics<br>
+ * 2.6 Nested Classes<br>
+ * 2.7 Exercises<br>
+ */
+package learn.dsajg6e.ch02oop;
