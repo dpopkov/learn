ec7a28e342d9262b025ad589ca530a98bd4ed189
Denis Popkov <pkvdenis@gmail.com>
2019-07-05 00:31:59 +0300

    introtdd: ch03writing: improve CafeTest.

--- introtdd/src/test/java/introtdd/ch02testing/CafeTest.java
+++ introtdd/src/test/java/introtdd/ch02testing/CafeTest.java
@@ -6,40 +6,43 @@ import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
 
 public class CafeTest {
+
+    public static final int ESPRESSO_BEANS = 7;
+
     @Test
     public void canBrewEspresso() {
         /* Given: Preconditions */
         Cafe cafe = new Cafe();
-        cafe.restockBeans(7);
+        cafe.restockBeans(ESPRESSO_BEANS);
         /* When: Behavior */
         Coffee coffee = cafe.brew(CoffeeType.Espresso);
         /* Then: Post-conditions */
-        assertThat(coffee.getType(), is(CoffeeType.Espresso));
-        assertThat(coffee.getMilk(), is(0));
-        assertThat(coffee.getBeans(), is(7));
+        assertThat("Wrong type of coffee", coffee.getType(), is(CoffeeType.Espresso));
+        assertThat("Wrong quantity of milk", coffee.getMilk(), is(0));
+        assertThat("Wrong quantity of beans", coffee.getBeans(), is(ESPRESSO_BEANS));
     }
 
     @Test
     public void brewingEspressoConsumesBeans() {
         // Given
         Cafe cafe = new Cafe();
-        cafe.restockBeans(7);
+        cafe.restockBeans(ESPRESSO_BEANS);
         // When
         cafe.brew(CoffeeType.Espresso);
         // Then
-        assertThat(cafe.getBeansInStock(), is(0));
+        assertThat("Wrong quantity of beans in stock", cafe.getBeansInStock(), is(0));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void negativeAmountThrowsException() {
         Cafe cafe = new Cafe();
-        cafe.restockBeans(-7);
+        cafe.restockBeans(-ESPRESSO_BEANS);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void latteRequiresMilk() {
         Cafe cafe = new Cafe();
-        cafe.restockBeans(7);
+        cafe.restockBeans(ESPRESSO_BEANS);
         cafe.restockMilk(1);
         cafe.brew(CoffeeType.Latte);
     }
