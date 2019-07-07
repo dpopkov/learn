c809dd4065f22cc2d70f7c4b075e9c5c02176f32
Denis Popkov <pkvdenis@gmail.com>
2019-07-08 00:57:56 +0300

    introtdd: ch03writing: improve CafeTest - add init method and constants.

--- introtdd/src/test/java/introtdd/ch02testing/CafeTest.java
+++ introtdd/src/test/java/introtdd/ch02testing/CafeTest.java
@@ -7,43 +7,70 @@ import static org.junit.Assert.*;
 
 public class CafeTest {
 
-    public static final int ESPRESSO_BEANS = 7;
+    private static final int ESPRESSO_BEANS = CoffeeType.Espresso.getRequiredBeans();
+    private static final int LATTE_BEANS = CoffeeType.Latte.getRequiredBeans();
+    private static final int LATTE_MILK = CoffeeType.Latte.getRequiredMilk();
+    private static final int NO_MILK = 0;
+    private static final int NO_BEANS = 0;
 
     @Test
     public void canBrewEspresso() {
         /* Given: Preconditions */
-        Cafe cafe = new Cafe();
-        cafe.restockBeans(ESPRESSO_BEANS);
+        Cafe cafe = cafeWithBeans(ESPRESSO_BEANS);
         /* When: Behavior */
         Coffee coffee = cafe.brew(CoffeeType.Espresso);
         /* Then: Post-conditions */
         assertThat("Wrong type of coffee", coffee.getType(), is(CoffeeType.Espresso));
-        assertThat("Wrong quantity of milk", coffee.getMilk(), is(0));
+        assertThat("Wrong quantity of milk", coffee.getMilk(), is(NO_MILK));
         assertThat("Wrong quantity of beans", coffee.getBeans(), is(ESPRESSO_BEANS));
+    }
+
+    @Test
+    public void canBrewLatte() {
+        /* Given: Preconditions */
+        Cafe cafe = cafeWithBeans(LATTE_BEANS);
+        cafe.restockMilk(LATTE_MILK);
+        /* When: Behavior */
+        Coffee coffee = cafe.brew(CoffeeType.Latte);
+        /* Then: Post-conditions */
+        assertThat("Wrong type of coffee", coffee.getType(), is(CoffeeType.Latte));
+        assertThat("Wrong quantity of milk", coffee.getMilk(), is(LATTE_MILK));
+        assertThat("Wrong quantity of beans", coffee.getBeans(), is(LATTE_BEANS));
     }
 
     @Test
     public void brewingEspressoConsumesBeans() {
         // Given
-        Cafe cafe = new Cafe();
-        cafe.restockBeans(ESPRESSO_BEANS);
+        Cafe cafe = cafeWithBeans(ESPRESSO_BEANS);
         // When
         cafe.brew(CoffeeType.Espresso);
         // Then
-        assertThat("Wrong quantity of beans in stock", cafe.getBeansInStock(), is(0));
+        assertThat("Wrong quantity of beans in stock", cafe.getBeansInStock(), is(NO_BEANS));
+    }
+
+    @Test
+    public void brewingLatteConsumesMilk() {
+        Cafe cafe = cafeWithBeans(LATTE_BEANS);
+        cafe.restockMilk(LATTE_MILK);
+        cafe.brew(CoffeeType.Latte);
+        assertThat("Wrong amount of milk", cafe.getMilkInStock(), is(NO_MILK));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void negativeAmountThrowsException() {
-        Cafe cafe = new Cafe();
-        cafe.restockBeans(-ESPRESSO_BEANS);
+        cafeWithBeans(-ESPRESSO_BEANS);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void latteRequiresMilk() {
-        Cafe cafe = new Cafe();
-        cafe.restockBeans(ESPRESSO_BEANS);
+        Cafe cafe = cafeWithBeans(ESPRESSO_BEANS);
         cafe.restockMilk(1);
         cafe.brew(CoffeeType.Latte);
+    }
+
+    private Cafe cafeWithBeans(int beans) {
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(beans);
+        return cafe;
     }
 }
