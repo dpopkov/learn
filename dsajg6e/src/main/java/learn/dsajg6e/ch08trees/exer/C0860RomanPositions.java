a47969a1ca35d7181f5af60bb7c0410491dd8620
Denis Popkov <pkvdenis@gmail.com>
2019-08-14 14:42:12 +0300

    dsajg6e: ch8: exercises: C-8.60 fix treeSize (not finished).

--- dsajg6e/src/main/java/learn/dsajg6e/ch08trees/exer/C0860RomanPositions.java
+++ dsajg6e/src/main/java/learn/dsajg6e/ch08trees/exer/C0860RomanPositions.java
@@ -4,12 +4,6 @@ import learn.dsajg6e.ch07list.positional.Position;
 import learn.dsajg6e.ch08trees.LinkedBinaryTree;
 
 public class C0860RomanPositions<E> extends LinkedBinaryTree<E> {
-    /* todo: Remove this method when the class is complete. */
-    public static void main(String[] args) {
-        LinkedBinaryTree<Integer> tree = new LinkedBinaryTree<>();
-        tree.numChildren(tree.root());
-        tree.size();
-    }
 
     public boolean isRomanPosition(Position<E> p) {
         Position<E> left = left(p);
@@ -20,17 +14,13 @@ public class C0860RomanPositions<E> extends LinkedBinaryTree<E> {
         return diff <= 5;
     }
 
-    public int treeSize(Position<E> p) {
-        return treeSize(p, 1);
-    }
-
-    // todo: watch at parameter 'sum'
-    private int treeSize(Position<E> p, int sum) {
+    private int treeSize(Position<E> p) {
+        int sum = 1;
         if (left(p) != null) {
-            sum += treeSize(left(p), 1);
+            sum += treeSize(left(p));
         }
         if (right(p) != null) {
-            sum += treeSize(right(p), 1);
+            sum += treeSize(right(p));
         }
         return sum;
     }
