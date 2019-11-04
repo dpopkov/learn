c60f6166ecace1b06761d01c46f75815b4e00049
Denis Popkov <pkvdenis@gmail.com>
2019-11-04 19:23:44 +0300

    dsajg6e: ch10maps: exercises: SkipList (start).

---
+++ dsajg6e/src/main/java/learn/dsajg6e/ch10maps/exer/AbstractSkipList.java
@@ -0,0 +1,78 @@
+package learn.dsajg6e.ch10maps.exer;
+
+import learn.dsajg6e.ch07list.positional.Position;
+
+public abstract class AbstractSkipList<E extends Comparable<E>> implements SkipList<E> {
+    protected static class Node<K extends Comparable<K>> implements Position<K> {
+        private K key;
+        private Node<K> up;
+        private Node<K> down;
+        private Node<K> left;
+        private Node<K> right;
+
+        public Node(K key) {
+            this.key = key;
+        }
+
+        @Override
+        public K getElement() throws IllegalStateException {
+            return key;
+        }
+
+        public Node<K> getUp() {
+            return up;
+        }
+
+        public void setUp(Node<K> up) {
+            this.up = up;
+        }
+
+        public Node<K> getDown() {
+            return down;
+        }
+
+        public void setDown(Node<K> down) {
+            this.down = down;
+        }
+
+        public Node<K> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<K> left) {
+            this.left = left;
+        }
+
+        public Node<K> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<K> right) {
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" + key + '}';
+        }
+    }
+
+    protected final E minKey;
+    protected final E maxKey;
+    protected Node<E> topLeft;
+    protected Node<E> topRight;
+    protected int size;
+
+    public AbstractSkipList(E minKey, E maxKey) {
+        this.minKey = minKey;
+        this.maxKey = maxKey;
+        topLeft = new Node<>(minKey);
+        topRight = new Node<>(maxKey);
+        topLeft.setRight(topRight);
+        topRight.setLeft(topLeft);
+    }
+
+    public int size() {
+        return size;
+    }
+}
---
+++ dsajg6e/src/main/java/learn/dsajg6e/ch10maps/exer/SkipList.java
@@ -0,0 +1,12 @@
+package learn.dsajg6e.ch10maps.exer;
+
+import learn.dsajg6e.ch07list.positional.Position;
+
+public interface SkipList<K extends Comparable<K>> {
+    Position<K> next(Position<K> p);
+    Position<K> prev(Position<K> p);
+    Position<K> above(Position<K> p);
+    Position<K> below(Position<K> p);
+    /** Returns position with the largest key that is less or equal to the specified key. */
+    Position<K> search(K key);
+}
