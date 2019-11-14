9278bdf882c8217dbb4f2609d98e8815ac6f8abe
Denis Popkov <pkvdenis@gmail.com>
2019-11-14 16:36:37 +0300

    dsajg6e: ch11searchtrees: start chapter

---
+++ dsajg6e/src/main/java/learn/dsajg6e/ch11searchtrees/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <h3>Chapter 11. Search Trees</h3>
+ *
+ * 11.1 Binary Search Trees<br>
+ * 11.2 Balanced Search Trees<br>
+ * 11.3 AVL Trees<br>
+ * 11.4 Splay Trees<br>
+ * 11.5 (2,4) Trees<br>
+ * 11.6 Red-Black Trees<br>
+ * 11.7 Exercises<br>
+ */
+package learn.dsajg6e.ch11searchtrees;
